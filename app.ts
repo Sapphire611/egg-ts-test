35dc624ab676c2eb467fedb43e64253bb7cbe94c
liuliyi <liuliyi@shgbit.com>
2022-07-25 11:56:10 +0800

    取消工作流

--- app.ts
+++ app.ts
@@ -2,13 +2,14 @@
  * @Author: Sapphire Liu
  * @Date: 2022-07-15 13:06:47
  * @LastEditors: Sapphire Liu
- * @LastEditTime: 2022-07-19 14:53:45
+ * @LastEditTime: 2022-07-20 14:27:02
  * @Description: app.ts
  */
 import { Application } from 'egg';
 
 export default (app: Application) => {
   app.beforeStart(async () => {
+    app.logger.warn('Warning');
     console.log('beforeStart ...');
   });
 
